8210899ec9edb8e54d08124ecda2d0f25b3bfa47
RekGRpth <rekgrpth@gmail.com>
2022-05-24 11:38:18 +0500

    fix

--- ngx_pq_module.c
+++ ngx_pq_module.c
@@ -870,10 +870,16 @@ static ngx_int_t ngx_pq_process_header(ngx_http_request_t *r) {
 
 static void ngx_pq_finalize_request(ngx_http_request_t *r, ngx_int_t rc) {
     ngx_log_debug1(NGX_LOG_DEBUG_HTTP, r->connection->log, 0, "rc = %i", rc);
+    ngx_http_upstream_t *u = r->upstream;
+    ngx_pq_data_t *d = u->peer.data;
+    ngx_pq_save_t *s = d->save;
+    ngx_log_debug1(NGX_LOG_DEBUG_HTTP, r->connection->log, 0, "s->rc = %i", s->rc);
+    switch (s->rc) {
+        case NGX_ERROR: s->rc = ngx_http_filter_finalize_request(r, NULL, NGX_HTTP_INTERNAL_SERVER_ERROR); return;
+    }
     if (!r->headers_out.status) r->headers_out.status = NGX_HTTP_OK;
     rc = ngx_http_send_header(r);
     if (rc == NGX_ERROR || rc > NGX_OK || r->header_only) return;
-    ngx_http_upstream_t *u = r->upstream;
     u->header_sent = 1;
     if (!u->out_bufs) return;
     u->out_bufs->next = NULL;
