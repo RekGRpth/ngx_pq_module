39582163412964cdf837b6f3cf24b832b4e50eb3
RekGRpth <rekgrpth@gmail.com>
2022-05-30 12:51:36 +0500

    op

--- ngx_pq_module.c
+++ ngx_pq_module.c
@@ -596,7 +596,7 @@ static ngx_int_t ngx_pq_result(ngx_pq_save_t *s, ngx_pq_data_t *d) {
             case PGRES_COMMAND_OK: rc = ngx_pq_command(s, d, res); break;
             case PGRES_COPY_OUT: rc = ngx_pq_copy(s, d); break;
             case PGRES_FATAL_ERROR: rc = ngx_pq_error(s, d, res); break;
-            case PGRES_PIPELINE_SYNC: ngx_log_debug0(NGX_LOG_DEBUG_HTTP, s->connection->log, 0, "PGRES_PIPELINE_SYNC"); PQclear(res); goto done;
+            case PGRES_PIPELINE_SYNC: ngx_log_debug0(NGX_LOG_DEBUG_HTTP, s->connection->log, 0, "PGRES_PIPELINE_SYNC"); break;
             case PGRES_TUPLES_OK: rc = ngx_pq_tuple(s, d, res); break;
             default: rc = ngx_pq_default(s, d, res); break;
         }
