584650da439a54bee4ad9be804e798c51af5aef4
RekGRpth <rekgrpth@gmail.com>
2022-06-08 12:48:56 +0500

    mv

--- ngx_pq_module.c
+++ ngx_pq_module.c
@@ -1299,6 +1299,60 @@ static ngx_http_module_t ngx_pq_ctx = {
     .merge_loc_conf = ngx_pq_merge_loc_conf
 };
 
+static char *ngx_pq_prepare_query_loc_ups_conf(ngx_conf_t *cf, ngx_command_t *cmd, ngx_array_t *queries) {
+    ngx_pq_query_t *query;
+    if (!queries->elts && ngx_array_init(queries, cf->pool, 1, sizeof(*query)) != NGX_OK) return "ngx_array_init != NGX_OK";
+    if (!(query = ngx_array_push(queries))) return "!ngx_array_push";
+    ngx_memzero(query, sizeof(*query));
+    ngx_pq_command_t *command;
+    if (ngx_array_init(&query->commands, cf->pool, 1, sizeof(*command)) != NGX_OK) return "ngx_array_init != NGX_OK";
+    ngx_str_t *str = cf->args->elts;
+    ngx_uint_t i = 1;
+    query->type = cmd->offset;
+    if (query->type & ngx_pq_type_prepare) {
+        if (ngx_http_script_variables_count(&str[i])) {
+            ngx_http_compile_complex_value_t ccv = {cf, &str[i], &query->name.complex, 0, 0, 0};
+            if (ngx_http_compile_complex_value(&ccv) != NGX_OK) return "ngx_http_compile_complex_value != NGX_OK";
+        } else query->name.str = str[i];
+        i++;
+    }
+    u_char *b = str[i].data;
+    u_char *e = str[i].data + str[i].len;
+    u_char *n = b;
+    u_char *s = n;
+    while (s < e) {
+        if (*s++ == '$') {
+            if ((*s >= 'a' && *s <= 'z') || (*s >= 'A' && *s <= 'Z') || *s == '_') {
+                if (!(command = ngx_array_push(&query->commands))) return "!ngx_array_push";
+                ngx_memzero(command, sizeof(*command));
+                command->str.data = n;
+                command->str.len = s - n - 1;
+                n = s;
+                while (s < e && ((*s >= '0' && *s <= '9') || (*s >= 'a' && *s <= 'z') || (*s >= 'A' && *s <= 'Z') || *s == '_')) s++;
+                if (!(command = ngx_array_push(&query->commands))) return "!ngx_array_push";
+                ngx_memzero(command, sizeof(*command));
+                command->str.data = n;
+                command->str.len = s - n;
+                n = s;
+                if (*s != '$') if ((command->index = ngx_http_get_variable_index(cf, &command->str)) == NGX_ERROR) return "ngx_http_get_variable_index == NGX_ERROR";
+            } else {
+                if (!(command = ngx_array_push(&query->commands))) return "!ngx_array_push";
+                ngx_memzero(command, sizeof(*command));
+                command->str.data = n;
+                command->str.len = s - n;
+                n = s;
+            }
+        }
+    }
+    if (n < s) {
+        if (!(command = ngx_array_push(&query->commands))) return "!ngx_array_push";
+        ngx_memzero(command, sizeof(*command));
+        command->str.data = n;
+        command->str.len = s - n;
+    }
+    return ngx_pq_argument_output_loc_conf(cf, query);
+}
+
 static char *ngx_pq_execute_loc_ups_conf(ngx_conf_t *cf, ngx_command_t *cmd, ngx_array_t *queries) {
     ngx_pq_query_t *query;
     if (!queries->elts && ngx_array_init(queries, cf->pool, 1, sizeof(*query)) != NGX_OK) return "ngx_array_init != NGX_OK";
@@ -1432,60 +1486,6 @@ static char *ngx_pq_pass_loc_conf(ngx_conf_t *cf, ngx_command_t *cmd, void *conf
     ngx_http_upstream_srv_conf_t *uscf = plcf->upstream.upstream;
     uscf->peer.init_upstream = ngx_pq_peer_init_upstream;
     return NGX_CONF_OK;
-}
-
-static char *ngx_pq_prepare_query_loc_ups_conf(ngx_conf_t *cf, ngx_command_t *cmd, ngx_array_t *queries) {
-    ngx_pq_query_t *query;
-    if (!queries->elts && ngx_array_init(queries, cf->pool, 1, sizeof(*query)) != NGX_OK) return "ngx_array_init != NGX_OK";
-    if (!(query = ngx_array_push(queries))) return "!ngx_array_push";
-    ngx_memzero(query, sizeof(*query));
-    ngx_pq_command_t *command;
-    if (ngx_array_init(&query->commands, cf->pool, 1, sizeof(*command)) != NGX_OK) return "ngx_array_init != NGX_OK";
-    ngx_str_t *str = cf->args->elts;
-    ngx_uint_t i = 1;
-    query->type = cmd->offset;
-    if (query->type & ngx_pq_type_prepare) {
-        if (ngx_http_script_variables_count(&str[i])) {
-            ngx_http_compile_complex_value_t ccv = {cf, &str[i], &query->name.complex, 0, 0, 0};
-            if (ngx_http_compile_complex_value(&ccv) != NGX_OK) return "ngx_http_compile_complex_value != NGX_OK";
-        } else query->name.str = str[i];
-        i++;
-    }
-    u_char *b = str[i].data;
-    u_char *e = str[i].data + str[i].len;
-    u_char *n = b;
-    u_char *s = n;
-    while (s < e) {
-        if (*s++ == '$') {
-            if ((*s >= 'a' && *s <= 'z') || (*s >= 'A' && *s <= 'Z') || *s == '_') {
-                if (!(command = ngx_array_push(&query->commands))) return "!ngx_array_push";
-                ngx_memzero(command, sizeof(*command));
-                command->str.data = n;
-                command->str.len = s - n - 1;
-                n = s;
-                while (s < e && ((*s >= '0' && *s <= '9') || (*s >= 'a' && *s <= 'z') || (*s >= 'A' && *s <= 'Z') || *s == '_')) s++;
-                if (!(command = ngx_array_push(&query->commands))) return "!ngx_array_push";
-                ngx_memzero(command, sizeof(*command));
-                command->str.data = n;
-                command->str.len = s - n;
-                n = s;
-                if (*s != '$') if ((command->index = ngx_http_get_variable_index(cf, &command->str)) == NGX_ERROR) return "ngx_http_get_variable_index == NGX_ERROR";
-            } else {
-                if (!(command = ngx_array_push(&query->commands))) return "!ngx_array_push";
-                ngx_memzero(command, sizeof(*command));
-                command->str.data = n;
-                command->str.len = s - n;
-                n = s;
-            }
-        }
-    }
-    if (n < s) {
-        if (!(command = ngx_array_push(&query->commands))) return "!ngx_array_push";
-        ngx_memzero(command, sizeof(*command));
-        command->str.data = n;
-        command->str.len = s - n;
-    }
-    return ngx_pq_argument_output_loc_conf(cf, query);
 }
 
 static char *ngx_pq_prepare_loc_conf(ngx_conf_t *cf, ngx_command_t *cmd, void *conf) {
